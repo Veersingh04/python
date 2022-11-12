9027f9639d0db61fbb44620df6998e252fad8e89
Veersingh04 <116937097+Veersingh04@users.noreply.github.com>
2022-11-12 09:25:31 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,14 @@
+#Take user input (string)
+#if the len of string is greater than 3
+#add 'ing' as a suffix in the original string
+#else print the same string given by user
+
+
+
+
+a = [1,2,3,4,]
+b = 5
+c = 15
+d = 20
+str = "a = {2}, b = {0}, c = {3}, d = {1}"
+print(str.format(c,d,b,a))
